6b20368df5ce204723d8a109e39e1aaa38aadb64
arqueon <arqueonautis@gmail.com>
2023-12-19 22:41:13 -0600

    casa

---
+++ config/hypr_2/scripts/LockScreen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONFIG="$HOME/.config/swaylock/config"
+
+#sleep 0.5s; swaylock --config ${CONFIG} & disown
+
+sleep 0.5s; swaylock -f -c 000000 & disown
---
+++ config/hypr_2/scripts/startup.sh
@@ -0,0 +1,11 @@
+kdeconnectd &
+insync start &
+copyq &
+#flameshot &
+#/usr/lib/geoclue-2.0/demos/agent &
+#redshift-gtk &
+variety &
+/opt/ExpanDrive/expandrive %U &
+ferdium &
+kdeconnect-indicator &
+#nm-applet --indicator
