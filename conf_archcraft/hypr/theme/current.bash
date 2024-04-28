95f2ee42c7fdfd5f36a2c91ee4890390918064b8
arqueon <arqueonautis@gmail.com>
2024-04-28 16:54:26 -0600

    c archcraft

---
+++ conf_archcraft/hypr/theme/current.bash
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Default Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#20262C'
+foreground='#f1fcf9'
+color0='#20262c'
+color1='#db86ba'
+color2='#74dd91'
+color3='#e49186'
+color4='#75dbe1'
+color5='#b4a1db'
+color6='#9ee9ea'
+color7='#f1fcf9'
+color8='#465463'
+color9='#d04e9d'
+color10='#4bc66d'
+color11='#db695b'
+color12='#3dbac2'
+color13='#825ece'
+color14='#62cdcd'
+color15='#e0e5e5'
+
+# Wallpaper
+wallpaper="$HOME/.config/hypr/wallpapers/wallpaper.png"
