6e6df74315234000dc0550203d5230ff7295b7bd
arqueon <arqueonautis@gmail.com>
2024-05-25 12:30:07 -0600

    c

--- bin/camaras.sh
+++ bin/camaras.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ffplay -f video4linux2 -framerate 60 -video_size 1920x1080 -input_format mjpeg /dev/video0
+ffplay -f video4linux2 -framerate 60 -video_size 1920x1080 -input_format mjpeg /dev/video2
 
